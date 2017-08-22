35a0a8dc5f58ae59510efae9e51b2571338a0147
anhgal <americkson.me@gmail.com>
2017-08-22 13:47:10 -0400

    Design updates and playground areas

--- src/js/critical.js
+++ src/js/critical.js
@@ -3,5 +3,11 @@ import loadFont from './utils/loadFont'
 loadFont([
 	{ family: 'Benton', suffix: 'regular' },
 	{ family: 'Benton', suffix: 'bold' },
-	{ family: 'Miller-Banner', suffix: 'regular' }
+	{ family: 'Miller', suffix: 'regular' },
+	{ family: 'Miller', suffix: 'bold' },
+	{ family: 'Miller-Banner', suffix: 'regular' },
+	{ family: 'Miller-Banner', suffix: 'italic' },
+	{ family: 'Miller-Text', suffix: 'italic' },
+    { family: 'Miller-Text', suffix: 'bold' },
+    { family: 'Miller-Text', suffix: 'regular' }
 ])
